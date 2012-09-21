ce66747751d1a3d1cb547ef1bd36d1f3911b2ee9
ecate <marine.serre@gmail.com>
2012-09-21 17:20:37 +0200

    afficher lessons actives uniquement

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -3,7 +3,7 @@ module LessonsHelper
   #return a list of 4 lessons with label "coup de coeur" - used in lessons#index for div "coup de coeur"
   def lessonscoupdecoeur
     @lessons_cdc= []
-    @cours= Syllabus.find_all_by_label_id(Label.find_by_name("Coup de coeur").id)
+    @cours= Syllabus.find_all_by_label_id_and_flag_actif(Label.find_by_name("Coup de coeur").id, true)
 
     @cours.each do |cours|
       cours.lessons.where('horaire > ?', DateTime.now.beginning_of_day).each do |less|
@@ -13,5 +13,15 @@ module LessonsHelper
       break if @lessons_cdc.count > 3
     end
     return @lessons_cdc
+  end
+
+  def lessonsactives
+    @lessons_actives= []
+    @lessons.each do |lesson|
+      if (lesson.syllabus.flag_actif == true && lesson.horaire > DateTime.now.beginning_of_day)
+        @lessons_actives << lesson
+      end
+    end
+    return @lessons_actives
   end
 end
